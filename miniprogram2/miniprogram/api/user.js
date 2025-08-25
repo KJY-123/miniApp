c750c93c2da66086c525a7b0a7ba8e807c0263c2
nanshan <826427687@qq.com>
2025-08-25 17:28:32 +0800

    study

---
+++ miniprogram2/miniprogram/api/user.js
@@ -0,0 +1,18 @@
+import http from '../utils/http'
+
+/**
+ * @description 登录接口
+ * @param {*} code 临时登录凭证
+ * @returns Promise
+ */
+export const reqLogin = (code) => {
+    return http.get(`/weixin/wxLogin/${code}`)
+}
+
+/**
+ * @description 获取用户信息
+ * @returns Promise
+ */
+export const reqUserInfo = () => {
+    return http.get(`/weixin/getUserInfo`)
+}
--- miniprogram2/miniprogram/pages/login/login.js
+++ miniprogram2/miniprogram/pages/login/login.js
@@ -1,2 +1,60 @@
-// pages/login/login.js
-Page({})
+import { toast } from '../../utils/extendApi'
+import { reqLogin, reqUserInfo } from '../../api/user'
+import { setStorage } from '../../utils/storage'
+import { ComponentWithStore } from 'mobx-miniprogram-bindings'
+import { userstore } from '../../stores/userstore'
+
+ComponentWithStore({
+    // 让页面和 store 建立联系
+    storeBindings: {
+        store: userstore,
+        fields: ['token', 'userInfo'],
+        actions: ['setToken', 'setUserInfo']
+    },
+    methods: {
+        // 授权登录
+        login() {
+            // 使用 wx.login 获取用户的临时登录凭证 code
+            wx.login({
+                success: async ({ code }) => {
+                    if (code) {
+                        // 获取到临时登录凭证 code 后，需要传递给开发者服务器
+                        const res = await reqLogin(code)
+                        console.log(res)
+
+                        // 接口出错，自定义模拟token
+                        setStorage('token', 'testToken')
+
+                        // 赋值 store
+                        this.setToken('testToken')
+
+                        this.getUserInfo()
+                    } else {
+                        toast({ title: '授权失败，请重新授权' })
+                    }
+                },
+                fail: () => {
+
+                }
+            })
+        },
+        
+        // 获取用户信息
+        async getUserInfo() {
+            // const res = await reqUserInfo()
+            // console.log('getUserInfo',res)
+
+            wx.getUserInfo({
+              lang: 'zh_CN',
+              withCredentials: true,
+              success: (result) => {
+                  console.log(result)
+                  setStorage(result.userInfo)
+                  this.setUserInfo(result.userInfo)
+              },
+              fail: (res) => {},
+              complete: (res) => {},
+            })
+        }
+    }
+})
